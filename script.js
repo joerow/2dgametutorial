f44064f69bac4649a72fe41c8f5b1421cae95134
joerow <joerow@gmail.com>
2022-10-01 16:23:48 +0800

    23:23

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ window.addEventListener("load", function () {
   canvas.width = 500;
   canvas.height = 500;
 
-  class InputHandler {}
+  class InputHandler {
+    constructor(game) {
+      this.game = game;
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "ArrowUp" && this.game.keys.indexOf(e.key) === -1) {
+          this.game.keys.push(e.key);
+        }
+        console.log(this.game.keys);
+      });
+      window.addEventListener("keyup", (e) => {
+        if (this.game.keys.indexOf(e.key) > -1) {
+          this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+        }
+        console.log(this.game.keys);
+      });
+    }
+  }
   class Projectile {}
   class Particle {}
   class Player {
@@ -33,6 +49,8 @@ window.addEventListener("load", function () {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
+      this.input = new InputHandler(this);
+      this.keys = [];
     }
     update() {
       this.player.update();
